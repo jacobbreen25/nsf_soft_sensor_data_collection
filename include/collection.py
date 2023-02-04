cdd4111269794d8c471d4c23feefb1fffb3adfe7
Jacob Breen <91704735+jacobbreen25@users.noreply.github.com>
2023-02-04 15:24:23 -0500

    Delete include directory

--- include/collection.py
+++
@@ -1,9 +0,0 @@
-import  tkinter as tk
-from tkinter import *
-from tkinter.filedialog import askdirectory
-from include import applib
-
-def connectBluetooth(button):
-   button['text'] = "Disconnect Bluetooth"
-   print("test") 
-   applib.switchButtonState(button)
