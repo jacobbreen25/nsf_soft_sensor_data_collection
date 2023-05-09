9d5535206aa983c2650267c9668094ba94535c03
Jacob Breen <91704735+jacobbreen25@users.noreply.github.com>
2023-05-09 16:49:55 -0400

    Add files via upload
    Signed-off-by: Jacob Breen <91704735+jacobbreen25@users.noreply.github.com>

--- NSF_Soft_Sensor_GUI.py
+++ NSF_Soft_Sensor_GUI.py
@@ -11,6 +11,9 @@ import asyncio
 from bleak import BleakScanner, BleakClient, backends 
 import csv
 import queue
+from functools import wraps
+
+glb_command = str
 
 class NSF_Gui(tk.Tk):
     """
@@ -19,12 +22,12 @@ class NSF_Gui(tk.Tk):
     def __init__(self, *args, **kwargs):
         self.experimentName = "qtpy"
         self.experimentPath = ""
-        self.samplerate = 120
+        self.samplerate = 60
         tk.Tk.__init__(self, *args, **kwargs)
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
-        
+        self.label = "Idle"
         self.mocapList = []
-
+        self.can_take = bool
         self.startTime = float
         self.endTesting = queue.Queue()
         self.bluetooth_command = queue.Queue()
@@ -33,25 +36,14 @@ class NSF_Gui(tk.Tk):
 
         self.bluetooth_active = queue.Queue()
 
-        #Thread(target=lambda : asyncio.run(self.__bluetooth_daemon()).start(), daemon=True).run()
+        
 
-        self.construct_gui()
-
-    async def bluetooth_daemon(self):
-        while self.bluetooth_active.empty():
-            if(self.bluetooth_command):
-                command = self.bluetooth_command.get()
-                if(command == "connect"):
-                    await self.connect_bluetooth()
-                elif(command == "capture"):
-                    await self.capture_bluetooth()
-            asyncio.sleep(2)
     """
         Construct GUI
 
         Variables are denoted by the naming convention below:
 
-        [Variable Type]VariableName ex. lFilePath
+        Variable Type]VariableName ex. lFilePath
 
         l = Label
         e = Entry
@@ -60,9 +52,10 @@ class NSF_Gui(tk.Tk):
         d = Dropdown Menu
         c = Checkbox
     """
-    def construct_gui(self):
+    async def construct_gui(self):
         self.title("NSF Soft Sensor Data Collection")
         self.resizable()
+        self.tasks = []
 
         f = [tk.Frame(master=self)]
         lHeader = tk.Label(master=f[-1], text="NSF Soft Sensor Data Collection", font=self.title_font)
@@ -92,30 +85,73 @@ class NSF_Gui(tk.Tk):
         f.append(tk.Frame(master=self))
         self.mocapCOM = tk.StringVar(master=f[-1])
         def acquireCOMList():
+            self.dMocapList['menu'].delete(0,'end')
             self.mocapList = ['None']
             self.mocapCOM.set('None')
             for p in serial.tools.list_ports.comports():
                 self.mocapList.append(p)
+            for option in self.mocapList:
+                self.dMocapList['menu'].add_command(label=option, command=tk._setit(self.mocapList, option))
             self.print("COM Options Reset\n")
         lMocapList = tk.Label(master=f[-1], text="Choose COM Port if using MOCAP ")
-        dMocapList = ttk.OptionMenu(f[-1], self.mocapCOM, 'None', *self.mocapList)
+        self.dMocapList = ttk.OptionMenu(f[-1], self.mocapCOM, 'None', *self.mocapList)
         bMocapReset = tk.Button(master=f[-1], text="Reset COM Options", command=lambda: acquireCOMList(), width=20)
         lMocapList.pack(side=tk.LEFT, pady=5)
-        dMocapList.pack(side=tk.LEFT, pady=5)
+        self.dMocapList.pack(side=tk.LEFT, pady=5)
         bMocapReset.pack(side=tk.LEFT, pady=5)
 
         # tk.Slider widget for hz here
 
         f.append(tk.Frame(master=self))
-        self.bBluetooth = tk.Button(master=f[-1], text="Connect Bluetooth", 
-            command=lambda: Thread(target=lambda : self.bluetooth_command.put("connect")).run(), 
-                                   width=50)
-        self.bTesting = tk.Button(master=f[-1], text="Begin Testing", 
-         command=lambda: Thread(target=lambda : self.bluetooth_command.put("capture")).run(), 
-                                width=50, state=tk.DISABLED)
+        lHz = tk.Label(master=f[-1], text="Slide to Desired Sample Rate (Hz):")
+        def sliderCommand(event):
+            self.samplerate = self.sHz.get()
+            self.print("Sample Rate set to " + str(self.samplerate) + "Hz\n")
+        self.sHz = tk.Scale(master=f[-1], from_= 60, to_=120, resolution=20, tickinterval=20, length=200, orient="horizontal", command=sliderCommand)
+        lHz.pack(side=tk.LEFT, pady=5)
+        self.sHz.pack(side=tk.LEFT, pady=5)
+        f.append(tk.Frame(master=self))
+        def connect():
+            global glb_command
+            glb_command = "connect"
+            return
+        
+        def capture():
+            global glb_command
+            glb_command = "capture"
+            if(self.bTesting['text'] == "Stop Testing"):
+                self.endTesting.put(1)
+            #print("Test")
+            return
+
+        self.bBluetooth = tk.Button(master=f[-1], text="Connect Bluetooth", command= lambda: Thread(connect()), width=50)
+        self.bTesting = tk.Button(master=f[-1], text="Begin Testing", command=lambda: Thread(capture()), width=50, state=tk.DISABLED)
         self.bBluetooth.pack(side=tk.LEFT, pady=5, padx=5)
         self.bTesting.pack(side=tk.LEFT, pady=5, padx=5)
         
+        f.append(tk.Frame(master=self))
+        bIdle = tk.Button(master=f[-1], text="Idle/Transition", command= lambda: Thread(setLabel(bIdle, "Idle")), state=tk.DISABLED)
+        self.disabledButton = bIdle
+        bWalking = tk.Button(master=f[-1], text="Walking", command= lambda: Thread(setLabel(bWalking, "Walking")))
+        bJogging = tk.Button(master=f[-1], text="Jogging", command= lambda: Thread(setLabel(bJogging, "Jogging")))
+        bSitToStand = tk.Button(master=f[-1], text="Sitting to Standing", command= lambda: Thread(setLabel(bSitToStand, "SitToStand")))
+        bWalkInc = tk.Button(master=f[-1], text="Walking Incline", command= lambda: Thread(setLabel(bWalkInc, "WalkingIncline")))
+        bWalkDec = tk.Button(master=f[-1], text="Walking Decline", command= lambda: Thread(setLabel(bWalkDec, "WalkingDecline")))
+        def setLabel(button, label):
+            self.disabledButton['state'] = tk.NORMAL
+            self.disabledButton = button
+            self.label = label
+            self.disabledButton['state'] = tk.DISABLED
+            return
+
+        bIdle.pack(side=tk.LEFT, pady=5, padx=5)
+        bWalking.pack(side=tk.LEFT, pady=5, padx=5)
+        bJogging.pack(side=tk.LEFT, pady=5, padx=5)
+        bSitToStand.pack(side=tk.LEFT, pady=5, padx=5)
+        bWalkInc.pack(side=tk.LEFT, pady=5, padx=5)
+        bWalkDec.pack(side=tk.LEFT, pady=5, padx=5)
+
+
         f.append(tk.Frame(master=self))
         hor = tk.Scrollbar(f[-1], orient="horizontal")
         hor.pack(side=tk.BOTTOM, fill=tk.X)
@@ -125,6 +161,9 @@ class NSF_Gui(tk.Tk):
         self.terminal.pack(side=tk.LEFT, pady=5)
         
         acquireCOMList()
+
+        for task in self.tasks:
+            await task
         for frame in f:
              frame.pack()
 
@@ -149,28 +188,38 @@ class NSF_Gui(tk.Tk):
     """
     async def connect_bluetooth(self, name : str):
         #self.bleDevice = backends.device.BLEDevice(0)
-        self.bleClient = None
+        #self.bleClient = None
         self.bBluetooth['state'] = tk.DISABLED
+        self.update()
         if(self.bBluetooth['text'] != "Disconnect Bluetooth"):
             devices = await BleakScanner.discover()
             for d in devices:
                 #self.print(str(d) + "\n")
                 if d.name == name:
-                    self.bleClient = BleakClient(d, disconnected_callback= lambda : self.disconnect_callback(), timeout=5.0)
-                    await self.bleClient.connect()
-                    self.bBluetooth['text'] = "Disconnect Bluetooth"
-                    self.bTesting['state'] = tk.NORMAL
-                    self.print("Connected to " + name + "\n")
+                    try:
+                        self.bleClient = BleakClient(d, disconnected_callback= self.disconnect_callback, timeout=3.0)
+                        await self.bleClient.connect()
+                        self.bBluetooth['text'] = "Disconnect Bluetooth"
+                        self.bTesting['state'] = tk.NORMAL
+                        self.print("Connected to " + name + "\n")
+                        break
+                    except Exception as e:
+                        self.bBluetooth['state'] = tk.NORMAL
+                        self.print("ERR: " + str(e) + "\n")
+                else:
+                    self.bleClient = None
         else:
             if (self.bleClient.is_connected):  
                 await self.bleClient.disconnect()
-                self.bBluetooth['text'] = "Connect Bluetooth"
-                self.fprint("Successfully disconnected from " + name + "\n")
+                #self.bBluetooth['text'] = "Connect Bluetooth"
+                #self.bTesting['state'] = tk.DISABLED
+                self.print(" " + name +" successfully!", "end-2c")
             else:
                 self.print("Wasn't connected at first!\n")
         if(self.bleClient is None):
             self.print("Could not find " + name + "\n")
-        self.bBluetooth['state'] = tk.NORMAL
+        self.bBluetooth ['state'] = tk.NORMAL
+        self.update()
 
     """
     
@@ -178,7 +227,9 @@ class NSF_Gui(tk.Tk):
     async def callback(self, characteristic, d : bytearray):
         if (self.can_take):
             data_array = d.decode().rstrip().split('\t')
+            data_array.append(self.label)
             self.clearln()
+            self.print("\n")
             self.print(data_array)
             self.write(data_array)
 
@@ -187,15 +238,17 @@ class NSF_Gui(tk.Tk):
     """    
     async def capture_bluetooth(self):
         self.bTesting['state'] = tk.DISABLED
+        self.update()
         if(self.bTesting['text'] == "Begin Testing"):
             self.print("Beginning Test...\n")
-            if (self.bleClient.is_connected()):
+            if (self.bleClient.is_connected):
                 with open(self.experimentPath + self.eFileName.get() + datetime.now().strftime("%m_%d_%Y-%H_%M") + ".csv",
                 'w+',newline='') as self.outfile:
                     self.outfileWritable = csv.writer(self.outfile)
                     try:
                         self.bTesting['text'] = "Stop Testing"
                         self.bTesting['state'] = tk.NORMAL
+                        self.update()
                         if(self.mocapBool.get()):
                             port_name = self.mocapCOM.get()
                             port_name  = port_name[:port_name.find('-') - 1]
@@ -203,33 +256,39 @@ class NSF_Gui(tk.Tk):
                                 serial = serial.Serial(port=port_name, baudrate=300)
                                 serial.write(1)
                                 serial.close()
-                        await self.bleClient.start_notify(self.dataChar, callback=lambda : self.callback)
+                        await self.bleClient.start_notify(self.dataChar, callback= self.callback)
                         await asyncio.sleep(0.02)
                         while self.endTesting.empty():
-                            await asyncio.sleep(0.05)
-                        await self.bleClient.stop_notify()
+                            #self.update()
+                            await asyncio.sleep(0.1)
+                            await asyncio.sleep(0)
+                        await self.bleClient.stop_notify(self.dataChar)
                     except Exception as e:
                         self.print(str(e) + "\n")
+                    self.print("\n")
                     self.endTesting.get()
                     self.outfile.close()
             else:
-                self.print("Bluetooth disconnected unexpectedly. Please try connecting again...\n")
                 self.bTesting['state'] = tk.DISABLED
                 self.bBluetooth['text'] = "Connect Bluetooth"
+                self.print("Bluetooth disconnected unexpectedly. Please try connecting again...\n")
         else:
-            self.endTesting.put(1)
+            self.print("Ending Test...")
             self.bTesting['text'] = "Begin Testing"
+            self.bTesting['state'] = tk.NORMAL
     
     """
     
     """
     def disconnect_callback(self, client):
-        print("disconnected")
+        self.bBluetooth['text'] = "Connect Bluetooth"
+        self.bTesting['state'] = tk.DISABLED
+        self.print("Disconnected from device\n")
         return
 
     def clearln(self):
         self.terminal['state'] = tk.NORMAL
-        self.terminal.replace()
+        #self.terminal.replace()
         self.terminal.delete("end-1l", tk.END)
         self.terminal['state'] = tk.DISABLED
 
@@ -237,13 +296,44 @@ class NSF_Gui(tk.Tk):
         self.terminal['state'] = tk.NORMAL
         self.terminal.insert(index, printable)
         self.terminal['state'] = tk.DISABLED
+        self.update()
 
     def write(self, writable : list):
         #writable.insert(0, datetime.time.microseconds())
         self.outfileWritable.writerow(writable)
-        
+    
+    async def bluetoothDaemon(self):
+        global glb_command
+        while(True):
+            #time.sleep(0.01)
+            self.update()
+            if (glb_command == "connect"):
+                glb_command = str
+                task = asyncio.create_task(self.connect_bluetooth("FlexibleSensorBLE-100"))
+                await task
+            elif (glb_command == "capture"):
+                glb_command = str
+                task = asyncio.create_task(self.capture_bluetooth())
+                await task
+            await asyncio.sleep(0)
+
+    async def clockDaemon(self):
+        while(True):
+            self.can_take = False
+            await asyncio.sleep(1/self.samplerate)
+            self.can_take = True
+            await asyncio.sleep(0)
+
 if __name__ == "__main__":
-    app = NSF_Gui()
-    #asyncio.run(app.bluetooth_daemon()).start()
-    app.mainloop()
+    async def asyncio_main():
+        app = NSF_Gui()
+        await app.construct_gui()
+        task1 = asyncio.create_task(app.bluetoothDaemon())
+        task2 = asyncio.create_task(app.clockDaemon())
+        await task1
+        await task2
+
+
+    
+    asyncio.run(asyncio_main())
         
